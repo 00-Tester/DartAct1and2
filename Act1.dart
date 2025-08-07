8a5aecfd70b35aa3cd028b2676d6a52e8cac0a53
John Mike Ignacio <166836142+00-Tester@users.noreply.github.com>
2025-08-07 09:37:15 +0000

    Activity 1: Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,14 @@
+// ACTIVITY #1
+
+// void main() {
+//   name = 'Alice';
+//   print("Welcome, " + name + '!');
+// }
+
+void main() {
+  dynamic name = 'Alice';
+  name = 25;
+  print("Welcome, $name!");
+}
+
+// This change is allowed without a compilation error because the Dynamic Integer acts as a temporary integer
