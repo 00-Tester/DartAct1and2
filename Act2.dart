a5e4b93d9c5920cbcb8c87b600702f8a4450fefb
John Mike Ignacio <166836142+00-Tester@users.noreply.github.com>
2025-08-07 09:48:40 +0000

    Activity 2: Understanding the Dart

--- Act2.dart
+++ Act2.dart
@@ -1,20 +1,20 @@
 // ACTIVITY #2
 
 void main() {
-  double result = calculateArea(7, 0, true);
-  print('The area of the square is ' + result.toString());
+  double result = calcPerimeter(7, 0, true);
+  print('The perimeter of the square is ' + result.toString());
   
-  result = calculateArea(10, 5, false);
-  print('The area of the rectangle is ' + result.toString());
+  result = calcPerimeter(10, 5, false);
+  print('The perimeter of the rectangle is ' + result.toString());
 }
 
-double calculateArea(double width, double height, bool isSquare) {
-  double area;
+double calcPerimeter(double width, double height, bool isSquare) {
+  double perimeter;
   if(isSquare)
   {
-    area = 4 * width;
+    perimeter = 4 * width;
   } else{
-  area = 2 * (width + height);
+  perimeter = 2 * (width + height);
   }
-  return area;
+  return perimeter;
 }
